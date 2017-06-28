01145077d4c5b280cfc01ea6898ce675dc98296e
juanjosec5 <sebasvelasco353@hotmail.com>
2017-06-28 04:44:43 -0500

    going to sleep now

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 var http = require('http');
-
+var port = 3006;
 http.createServer(function (req, res) {
 
   //A parsed url to work  with in case there are parameters
-  var _url;
+  var url = req.url;
 
   //in case the client uses lower case for methods
   req.method = req.method.toUpperCase();
   console.log(req.method + '  ' + req.url);
-  res.end('The current time is ' + Date.now());
-}).listen(3001, '127.0.0.1');
+  if (req.method !== 'GET') {
+    res.writeHead(501, {
+      'Content-Type': 'text/plain',
+    });
+    return res.end(req.metohd + 'is not implemented by this server');
+  }
 
-console.log('Server running at 3000.');
+  if (req.url == '/employees') {
+    //returns a list of employees
+    res.writeHead(200);
+    return res.end('employee list');
+  } else if (req.url == '/employees/') {
+    //find a single employee
+    url.id  = url.split('/')[2];
+    res.writeHead(200);
+    return res.end(' a single employee');
+  } else {
+    //try to send a static file
+    res.writeHead(200);
+    res.end('Static file');
+  }
+}).listen(port, '127.0.0.1');
+console.log('Server running at ' + port);
